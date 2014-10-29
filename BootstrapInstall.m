9a4017e1ae311d06ed72dc3663202eb155348c19
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-10-29 14:18:45 +0100

    Use BootstrapInstaller

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -1,42 +1,18 @@
 (* ::Package:: *)
 
-Module[
-	{$PIImportResult}
-	,
-	
-	Quiet[
-		$PIImportResult = Needs["ProjectInstaller`"],
-		{Get::noopen, Needs::nocont}
-	];
-	
-	If[$PIImportResult === $Failed,
-		Print["ProjectInstaller not found, installing it:"];
-		Print @ Import[
-			"https://raw.github.com/lshifr/ProjectInstaller/master/BootstrapInstall.m"
-		];
-		$PIImportResult = Needs["ProjectInstaller`"];
-	];
+Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1.1/BootstrapInstaller.m"]
 
-	If[$PIImportResult === $Failed,
-		Print[
-			"Unable to load ProjectInstaller.\n",
-			"Please ",
+
+BootstrapInstall[
+	"OptionsUtilities",
+	"https://github.com/jkuczm/MathematicaOptionsUtilities/releases/download/v0.1.0/OptionsUtilities.zip",
+	"AdditionalFailureMessage" ->
+		Sequence[
+			"You can ",
 			Hyperlink[
 				"install OptionsUtilities package manually",
 				"https://github.com/jkuczm/MathematicaOptionsUtilities#manual-installation"
 			],
-			".\n",
-			"We would be grateful for ",
-			Hyperlink[
-				"reporting this issue",
-				"https://github.com/jkuczm/MathematicaOptionsUtilities/issues"
-			],
 			"."
-		];
-	(* else *),
-		Print["Installing OptionsUtilities:"];
-		Print @ ProjectInstaller`ProjectInstall @ URL[
-			"https://github.com/jkuczm/MathematicaOptionsUtilities/releases/download/v0.1.0/OptionsUtilities.zip"
-		];
-	];
+		]
 ]
